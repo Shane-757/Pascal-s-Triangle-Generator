2e04ad7ae103708614d64239d405d96405cb3bd1
Shane-757 <underwoodshane@ymail.com>
2023-04-10 12:29:40 -0400

    update

--- generateTriangle.js
+++ generateTriangle.js
@@ -1,5 +1,14 @@
 function generateTriangle(userInput) {
   const arr = [];
+  
+  for (let i = 0; i < userInput; i++) {
+    arr[i] = [];
+    arr[i][0] = 1;
+    arr[i][i] = 1;
+    for (let j = 1; j < i; j++) {
+      arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+    }
+  }
   return arr;
 }
 
